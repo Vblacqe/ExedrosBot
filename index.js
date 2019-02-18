9a16e54dffc5a70b92e3cd4679a0dced747f4c9e
unknown <vblackgp123@gmail.com>
2019-02-18 18:40:08 +0100

    first

---
+++ index.js
@@ -0,0 +1,21 @@
+const Commando = require('discord.js-commando');
+const bot = new Commando.Client();
+const TOKEN = 'NTQ2NzYzODcxMTEzOTY5Njc0.D0xMmA.p9BTEeo7oAXeDJw0mpg_pStBOpE';
+const cmdp = 'e!';
+
+bot.registry.registerGroup('connectc', 'Connectc');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands")
+
+bot.on('ready', function(){
+    console.log("Ready");
+});
+
+bot.on("message", function(message){
+    if(message.content == cmdp + 'czesc'){
+        message.reply('No czesc!');
+        message.channel.send('Co tam? ' + message.author + ' Nadal robisz tego bota?');
+    }
+});
+
+bot.login(TOKEN);
